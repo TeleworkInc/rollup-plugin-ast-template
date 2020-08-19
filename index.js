f28d6e4341134a9733a3f24c4e1b1e659a45d442
Christian Lewis <ctjl96@gmail.com>
2020-08-19 15:43:01 -0500

    Updated template

--- index.js
+++ index.js
@@ -6,11 +6,52 @@
  * Entry point for module.
  */
 
-const path = require('path');
-const fs = require('fs');
 const babel = require('@babel/core');
-
 const shebangPattern = /^#!.*/;
+
+/**
+ * Example from rollup-plugin-disable-packages, which replaces imports of a
+ * given list of packages with empty objects (`{}`).
+ *
+ * @param {...string} disabledPackages
+ * @return {object}
+ */
+const ASTReplacements = (...disabledPackages) => ({
+  visitor: {
+    CallExpression(path) {
+      if (
+        path.node.callee.name === 'require' &&
+          path.node.arguments.length === 1 &&
+          disabledPackages.includes(path.node.arguments[0].value)
+      ) {
+        const newNode = babel.types.objectExpression([]);
+        path.replaceWith(newNode);
+      }
+    },
+    ImportDeclaration(path) {
+      if (
+        path.node.source.type === 'StringLiteral' &&
+          disabledPackages.includes(path.node.source.value)
+      ) {
+        const newNodes = [];
+        for (const specifier of path.node.specifiers) {
+          const disableName = specifier.local.name;
+          const newNode = babel.types.variableDeclaration(
+              'const',
+              [
+                babel.types.variableDeclarator(
+                    babel.types.identifier(disableName),
+                    babel.types.objectExpression([]),
+                ),
+              ],
+          );
+          newNodes.push(newNode);
+        }
+        path.replaceWithMultiple(newNodes);
+      }
+    },
+  },
+});
 
 /**
  * Import `empty` instead of a given module.
@@ -31,47 +72,7 @@ const disablePackages = (...disabledPackages) => {
       code = code.replace(shebangPattern, '');
 
       const output = babel.transformSync(code, {
-        plugins: [
-          // your first babel plugin 😎😎
-          function myCustomPlugin() {
-            return {
-              visitor: {
-                CallExpression(path) {
-                  if (
-                    path.node.callee.name === 'require' &&
-                    path.node.arguments.length === 1 &&
-                    disabledPackages.includes(path.node.arguments[0].value)
-                  ) {
-                    const newNode = babel.types.objectExpression([]);
-                    path.replaceWith(newNode);
-                  }
-                },
-                ImportDeclaration(path) {
-                  if (
-                    path.node.source.type === 'StringLiteral' &&
-                    disabledPackages.includes(path.node.source.value)
-                  ) {
-                    const newNodes = [];
-                    for (const specifier of path.node.specifiers) {
-                      const disableName = specifier.local.name;
-                      const newNode = babel.types.variableDeclaration(
-                          'const',
-                          [
-                            babel.types.variableDeclarator(
-                                babel.types.identifier(disableName),
-                                babel.types.objectExpression([]),
-                            ),
-                          ],
-                      );
-                      newNodes.push(newNode);
-                    }
-                    path.replaceWithMultiple(newNodes);
-                  }
-                },
-              },
-            };
-          },
-        ],
+        plugins: [ASTReplacements(...disabledPackages)],
       });
 
       return output.code;
